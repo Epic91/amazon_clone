a9f8259306ef0c8e733c4eb08a0357f96e916fcf
Evelyn Hernandez <hernandeze791@gmail.com>
2021-04-22 16:01:57 -0500

    first commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+    return (
+        <div>
+            <HeaderLogo />
+            
+        </div>
+    )
+}
+
+export default Header
+
+const HeaderLogo =  styled.div `
+    height: 60px;
+    background-color: #0F1111;
+`
