5e6e201cf77b8d45a07abbd45055a5e27d5dcfb2
Evelyn Hernandez <hernandeze791@gmail.com>
2021-04-22 17:06:25 -0500

    added logo and search bar

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,69 @@
 import React from 'react'
 import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
+
     return (
-        <div>
-            <HeaderLogo />
+
+        <Container>
+            <HeaderLogo>
+                <img src={"https://i.imgur.com/7I9Was5.png"}/>
+            </HeaderLogo>
+
+            <HeaderOptionAddress>
+                <OptionLineOne>Hello,</OptionLineOne>
+                <OptionLineTwo>Select your address</OptionLineTwo>
+            </HeaderOptionAddress>
+
+            <HeaderSearch>
+                <HeaderSearchInput type="text" />
+
+                <HeaderSearchIconContainer>
+                    {/* <SearchIcon /> */}
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
             
-        </div>
+        </Container>
     )
 }
 
 export default Header
 
-const HeaderLogo =  styled.div `
+const Container = styled.div `
     height: 60px;
     background-color: #0F1111;
+    display: flex;
+    align-items: center;
+    color: white;
 `
+
+const HeaderLogo =  styled.div `
+    img {
+        width: 100px;
+        margin-left: 11px;
+    }
+`
+
+const HeaderOptionAddress = styled.div `
+
+`
+
+const OptionLineOne = styled.div `
+
+`
+
+const OptionLineTwo = styled.div `
+
+`
+
+const HeaderSearch = styled.div `
+    display: flex;
+
+`
+
+const HeaderSearchInput = styled.input `
+
+`
+
+const HeaderSearchIconContainer = styled.div ``
